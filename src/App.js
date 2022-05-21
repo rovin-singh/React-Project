99bfbcfc2b5ad53650ccf1706e32159cfff86dc9
rovin-singh <91409265+rovin-singh@users.noreply.github.com>
2022-05-21 15:27:02 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./styles.css";
+import React from "react";
 import Navbar from "./components/Navbar";
 import Offers from "./components/Offers";
 import Filters from "./components/Filters";
@@ -6,19 +7,47 @@ import Restaurants from "./components/Restaurants";
 import userInfo from "./data/userInfo";
 import offers from "./data/offers";
 import restaurants from "./data/restaurants.json";
+const filters = {
+  1: "Cost High to Lost",
+  2: "Cost Lost to High",
+  3: "Ratings",
+  4: "Delivery Time",
+  5: "Relevance"
+};
 function App() {
-  const filters = {
-    1: "Cost High to Lost",
-    2: "Ratings",
-    3: "Delivery Time",
-    4: "Relevance"
+  const [filterBy, setFilterBy] = React.useState("");
+  const [data, setData] = React.useState(restaurants);
+
+  const updateFilter = (newFilter) => {
+    switch (newFilter) {
+      case "1": {
+        setFilterBy("1");
+        data.sort((a, b) => a.costfortwo - b.costfortwo);
+        setData([...data]);
+        break;
+      }
+      case "2": {
+        setFilterBy("2");
+        data.sort((a, b) => b.costfortwo - a.costfortwo);
+        setData([...data]);
+        break;
+      }
+      default: {
+        setData(restaurants);
+        break;
+      }
+    }
   };
   return (
     <div className="App">
       <Navbar {...userInfo} />
       <Offers offer={offers} />
-      <Filters filters={filters} />
-      <Restaurants restaurants={restaurants} />
+      <Filters
+        filters={filters}
+        currentFilteredBy={filterBy}
+        updateFilter={updateFilter}
+      />
+      <Restaurants restaurants={data} />
     </div>
   );
 }
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-const Filters = ({ filters }) => {
+const Filters = ({ filters, currentFilteredBy, updateFilter }) => {
   return (
     <div className="App">
       <div class="container restaurants">
         <h1>50 restaurants</h1>
         <div class="restaurant-options">
-          {Object.values(filters).map((value) => (
-            <div class="restaurant-option">{value}</div>
+          {Object.entries(filters).map(([key, value]) => (
+            <div
+              key={key}
+              className={`restaurant-option ${
+                currentFilteredBy === key ? "underline" : "false"
+              }`}
+              onClick={() => updateFilter(key)}
+            >
+              {value}
+            </div>
           ))}
         </div>
       </div>
